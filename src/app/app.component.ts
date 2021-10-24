75272b2f723f8c7a3282dfcaebc7fc73e45805ee
Alyona <alyona_startseva.tk@mail.ru>
2021-10-24 22:52:53 +0700

    Added modal window for library creation Created a separate component for the modal window, added a simple form for data, created a separate class for the entity library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Library} from "./Classes/Library";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'libraries-angular-test-app';
+
+  librariesArray: Library[] = [
+                                new Library("2-я детская библиотека Петроградского района",
+                                  "Санкт-Петербург",
+                                  "Татарский пер., 1",
+                                  "Санкт-Петербургское государственное бюджетное учреждение «Централизованная библиотечная система Петроградского района»",
+                                  "Библиотека открылась в 1955 году. Дом, в котором она располагается, возведен по проекту известнейшего петербургского архитектора Павла Мульханова в стиле модерн в 1909 году."
+                                  )
+  ];
+
+  /*Modal*/
+  newLibrary: Library = new Library("", "", "", "", "");
+
+  addLibrary() {
+    this.librariesArray.push(new Library(this.newLibrary.name, this.newLibrary.locale, this.newLibrary.address, this.newLibrary.organizationName, this.newLibrary.description));
+
+    console.log(this.librariesArray)
+  }
+
 }
