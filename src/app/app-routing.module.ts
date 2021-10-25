0f0ede2d769b4593e0a5421a248d84475bfcdbda
Alyona <alyona_startseva.tk@mail.ru>
2021-10-26 02:03:03 +0700

    Added scaffolded component and implemented routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ListOfLibrariesComponent } from "./list-of-libraries/list-of-libraries.component";
+import { LibraryDetailsComponent } from "./library-details/library-details.component";
 
 const routes: Routes = [
     {
       path: '',
       component: ListOfLibrariesComponent
+    },
+    {
+      path: 'details/:library',
+      component: LibraryDetailsComponent
     }
   ];
 
