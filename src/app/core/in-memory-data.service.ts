7716c15a4176697ac9bf2c7648b2657d6e67b2b1
Alyona <alyona_startseva.tk@mail.ru>
2021-11-26 00:23:54 +0700

    Fixed bag with additing libraries

--- src/app/core/in-memory-data.service.ts
+++ src/app/core/in-memory-data.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
-import { IEmployee } from './interfaces/employee';
 
 import { ILibrary } from './interfaces/Library';
 
@@ -28,35 +27,12 @@ export class InMemoryDataService implements InMemoryDbService {
                                       description: "Библиотека открылась в 1955 году. Дом, в котором она располагается, возведен по проекту известнейшего петербургского архитектора Павла Мульханова в стиле модерн в 1909 году.",
                                     }
                                   ];
-  const employees: IEmployee[] = [
-                                    {
-                                      id: 11,
-                                      idLibrary: 1,
-                                      firstName: 'Иван',
-                                      lastName: 'Иванов',
-                                      position: 'Библиотекарь',
-                                      address: 'г.Новосибирск, ул.Ленина, 3а',
-                                      startedWork: '12.12.2012'
-                                    },
-                                    {
-                                      id: 22,
-                                      idLibrary: 2,
-                                      firstName: 'Иван',
-                                      lastName: 'Иванов',
-                                      position: 'Библиотекарь',
-                                      address: 'г.Новосибирск, ул.Ленина, 3а',
-                                      startedWork: '12.12.2012'
-                                    }
-                                ]
-    return { libraries, employees };
+
+    return { libraries };
   }
 
-  genIdLibraries(libraries: ILibrary[]): number {
+  genId(libraries: ILibrary[]): number {
     return libraries.length > 0 ? Math.max(...libraries.map(library => library.id)) + 1 : 1;
-  }
-
-  genIdEmployees(employees: IEmployee[]): number {
-    return employees.length > 0 ? Math.max(...employees.map(employee => employee.id)) + 1 : 1;
   }
 
 }
